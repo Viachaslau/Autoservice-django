0aad9634572052570911ef1ff24a11d375a8c7d2
Viachaslau <slava.golovchik1903@gmail.com>
2023-04-13 16:54:14 +0200

    current version

--- MalAuto/models.py
+++ MalAuto/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from datetime import datetime
 from django.core.validators import MinLengthValidator
-from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
